79cab787b56a3acbff604feea5ed2aec411d6fb4
Your Name01~ <you@example.com>
2023-03-09 10:43:38 +0600

    Winner Declaration algorithm added

--- src/index.js
+++ src/index.js
@@ -24,9 +24,13 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if(calculateWinner(squares) || squares[i]){
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
-    const updated_state = { squares: squares, xIsNext: !(this.state.xIsNext) };
-    this.setState(updated_state);
+    this.setState({
+      squares:squares, xIsNext: ! (this.state.xIsNext)
+    });
   }
 
   renderSquare(i) {
@@ -34,8 +38,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner){
+      status = "Winner: " + winner;
+    }else{
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : '0');
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -89,21 +98,22 @@ root.render(<Game />);
 ]
 */
 
-function calculateWinner(squares){
+function calculateWinner(squares) {
   const lines = [
-    [0,1,2], [3,4,5],[6,7,8],
-    [0.3,6], // Horizontal
-    [1,4,7], // Horizontal
-    [3,5,8], // Horizontal
-    [0,4,8], // Diagonal
-    [6,4,2] // Diagonal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
-
-  for(let i = 0; i < lines.length; i++){
-    const [a,b,c] = lines[i];
-    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
-    return null;
   }
+  return null;
 }
